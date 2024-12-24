27af20959882f0fbb7da13663999af70d2f87fe5
sBahre <shubham0bahre@gmail.com>
2024-12-24 20:47:45 +0530

    Added new angular project

---
+++ ngApp/src/app/special-events/special-events.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-special-events',
+  imports: [],
+  templateUrl: './special-events.component.html',
+  styleUrl: './special-events.component.scss'
+})
+export class SpecialEventsComponent {
+
+}
